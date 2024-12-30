657feb6d3ae0bfb9edd5fab35868a834fca3a618
Contributor 5b2311c3 <contributor-5b2311c3@users.noreply.github.com>
2024-12-31 04:45:05 +0900

    Fixed the issue for unregister API

--- pages/api/devices/delete.ts
+++ pages/api/devices/delete.ts
@@ -40,6 +40,11 @@ export default async function handler(
       return;
     }
 
+    if (!exists.address || exists.address !== session.user.address) {
+      res.status(401).json({ message: 'Unauthorized' });
+      return;
+    }
+
     const product = (await db
       .collection('products')
       .findOne({ key: miner_key.split('-')[0] })) as Product;
