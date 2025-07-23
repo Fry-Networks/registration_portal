40d38e2f8e7e2320d9163a1616dec9f0854f7c0f
Contributor 5b2311c3 <contributor-5b2311c3@users.noreply.github.com>
2025-07-23 17:23:51 +0900

    fixed the ',' bug

--- app/modalcontext.tsx
+++ app/modalcontext.tsx
@@ -25,7 +25,7 @@ export const ModalProvider: React.FC<{ children: React.ReactNode }> = ({
     claim: false,
     delete: false,
     generate_wallet: false,
-    withdraw_all: false
+    withdraw_all: false,
     fryConversion: false,
     // Add other modals here as needed
   });
